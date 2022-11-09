538d3dca75d9cf2eef443dd0ee07064ba65bbe91
Michael Thomas <mike.ahpong@gmail.com>
2022-11-09 17:29:03 +0800

    added create function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,29 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # No need for app/views/restaurants/create.html.erb
+    # raise
+    redirect_to task_path(@task)
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task]) # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root        to: "tasks#index"
 
   get "tasks" , to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
   get "tasks/:id" , to: "tasks#show", as: :task
+  post '/tasks', to: "tasks#create"
 end
